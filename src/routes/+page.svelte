45a9dc23a436ffa5ca50d19fae5ebfb84c4b5c0b
Daniils Petrovs <thedanpetrov@gmail.com>
2023-01-01 23:28:33 +0100

    Bootstrap DaisyUI

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,38 @@
-<h1 class="text-3xl font-bold underline">
-  Watson Industries. Check back later...
-</h1>
+<div class="flex flex-col">
+
+	<div class="hero bg-primary py-5">
+		<div class="hero-content text-center text-accent">
+			<div class="max-w-md mx-auto">
+				<h1 class="text-5xl font-bold my-3">Watson Industries</h1>
+				<p class="py-6">Innovation knows no bounds at Watson Industries, where we push the limits of what's possible in rail, ametaverse, NFATs, Artificial Intelligence, groundpounding, and even time travel. Join us on the cutting edge of progress.</p>
+				<span class="text-center">
+					<button class="btn btn-secondary">Join Us</button>
+				</span>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="hero bg-accent">
+  <div class="hero-content flex-col lg:flex-row">
+    <img src="https://placeimg.com/260/400/arch" class="max-w-sm rounded-lg shadow-2xl" />
+    <div class="text-primary">
+      <h1 class="text-5xl font-bold">Watson Railways</h1>
+      <p class="py-6">Experience the future of transportation with Watson Railways, a subsidiary of Watson Industries. With cutting-edge technology and a dedication to safety and efficiency, we are revolutionizing the way we travel by rail.</p>
+      <button class="btn btn-primary">Get Started</button>
+    </div>
+  </div>
+</div>
+
+<div class="hero bg-primary">
+  <div class="hero-content text-center text-accent">
+    <div class="max-w-md">
+      <h1 class="text-5xl font-bold">Our Partners</h1>
+      <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+      
+			<h2 class="text-xl">Kiara Fried Phoenix</h2>
+			<h2 class="text-xl">Tako Eats</h2>
+			<h2 class="text-xl">City of Wattropolis</h2>
+    </div>
+  </div>
+</div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,30 @@
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'ame-light-yellow': '#F2D7B6',
+        'ame-dark-brown': '#40302b',
+        'ame-red': '#BF545D'
+      }
+    },
   },
-  plugins: [],
+  daisyui: {
+    themes: [
+      {
+        ametheme: {
+          "primary": "#f2d7b6",
+          "secondary": "#bf545d",
+          "accent": "#40302b",
+          "neutral": "#191D24",
+          "base-100": "#2A303C",
+          "info": "#60b6bf",
+          "success": "#36D399",
+          "warning": "#FBBD23",
+          "error": "#F87272",
+        },
+      },
+    ],
+  },
+  plugins: [require("@tailwindcss/typography"), require('daisyui')],
 }
