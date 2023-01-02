c4c742b922da959c701e318bdebee806d4a2c869
Daniils Petrovs <thedanpetrov@gmail.com>
2023-01-03 00:55:03 +0100

    Bootstrap a landing page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,8 +2,38 @@
     import type { LayoutData } from './$types';
     import "../app.css";
 
+    import { Github, Twitter } from '@steeze-ui/simple-icons'
+    import { Icon } from '@steeze-ui/svelte-icon'
+
     export let data: LayoutData;
 </script>
+
+<div class="navbar bg-base-100">
+  <div class="navbar-start">
+    <div class="dropdown">
+      <label tabindex="0" class="btn btn-ghost btn-circle">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
+      </label>
+      <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+        <li><a href="/">Homepage</a></li>
+        <li><a>Portfolio</a></li>
+        <li><a href="about">About</a></li>
+      </ul>
+    </div>
+  </div>
+  <div class="navbar-center">
+    <a class="btn btn-ghost normal-case text-xl text-info" href="/">Watson Industries</a>
+  </div>
+  <div class="navbar-end">
+    <a type="button" href="https://github.com/watsonindustries" class="btn btn-ghost btn-circle">
+        <Icon src={Github} size="24px" theme="mini" class="fill-primary h-5 w-5" />
+    </a>
+    <a type="button" href="https://twitter.com/watsonameliaEN" class="btn btn-ghost btn-circle">
+      <Icon src={Twitter} size="24px" theme="mini" class="fill-primary h-5 w-5" />
+  </a>
+    
+  </div>
+</div>
 
 <slot />
 
@@ -20,7 +50,7 @@
       <a class="link link-hover">About us</a>
       <a class="link link-hover">Contact</a>
       <a class="link link-hover">Jobs</a>
-      <a class="link link-hover">Press kit</a>
+      <a class="link link-hover" href="/credits" >Credits</a>
     </div> 
     <div>
       <span class="footer-title">Legal</span> 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <div class="flex flex-col">
 
-	<div class="hero bg-primary py-5">
+	<div class="hero bg-primary py-20">
 		<div class="hero-content text-center text-accent">
 			<div class="max-w-md mx-auto">
 				<h1 class="text-5xl font-bold my-3">Watson Industries</h1>
@@ -13,26 +13,55 @@
 	</div>
 </div>
 
-<div class="hero bg-accent">
+<div class="hero bg-accent py-20">
   <div class="hero-content flex-col lg:flex-row">
     <img src="https://placeimg.com/260/400/arch" class="max-w-sm rounded-lg shadow-2xl" />
-    <div class="text-primary">
+    <div class="text-primary px-5">
       <h1 class="text-5xl font-bold">Watson Railways</h1>
       <p class="py-6">Experience the future of transportation with Watson Railways, a subsidiary of Watson Industries. With cutting-edge technology and a dedication to safety and efficiency, we are revolutionizing the way we travel by rail.</p>
-      <button class="btn btn-primary">Get Started</button>
+      <button class="btn btn-primary">Learn more</button>
     </div>
   </div>
 </div>
 
-<div class="hero bg-primary">
+<div class="hero bg-primary py-20">
   <div class="hero-content text-center text-accent">
-    <div class="max-w-md">
-      <h1 class="text-5xl font-bold">Our Partners</h1>
-      <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+
+    <div >
+			<div class="mx-auto">
+				<h1 class="text-5xl font-bold">Our Partners</h1>
+				<p class="py-6">We would be nothing without our amazing partners, who supply us with nutrition and energy to keep pounding those grounds.</p>
+			</div>
       
-			<h2 class="text-xl">Kiara Fried Phoenix</h2>
-			<h2 class="text-xl">Tako Eats</h2>
-			<h2 class="text-xl">City of Wattropolis</h2>
+			<div class="flex md:flex-row flex-col">
+				<div class="md:mx-2 my-3 mx-auto">
+					<img src="/logos/tako-eats.png" alt="Tako Eats logo" class="h-80" />
+				</div>
+				
+				<div class="md:mx-2 my-3 mx-auto">
+					<a href="https://vivalakiara.com">
+						<img src="/logos/kfp.png" alt="KFP logo with black background" class="h-80" />
+					</a>
+				</div>
+
+				<div class="md:mx-2 my-3 mx-auto">
+					<a href="https://uber-sheep.com">
+						<img src="/logos/uber-sheep-dark.png" alt="Uber Sheep logo by totangtotan" class="h-80" />
+					</a>
+				</div>
+			</div>	
+    </div>
+
+  </div>
+</div>
+
+<div class="hero bg-accent py-20">
+  <div class="hero-content flex-col lg:flex-row-reverse text-primary">
+    <img src="https://placeimg.com/260/400/arch" class="max-w-sm rounded-lg shadow-2xl" />
+    <div>
+      <h1 class="text-5xl font-bold">Open Source</h1>
+      <p class="py-6">At Watson Industries, we believe in the power of collaboration and sharing knowledge. That's why we make our projects open source, allowing others to contribute, learn, and innovate with us.</p>
+      <button class="btn btn-primary">Browse Projects</button>
     </div>
   </div>
 </div>
