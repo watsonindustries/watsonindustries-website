bb404c4120f9ed879d7e282b60bcccc20976ba4d
Daniils Petrovs <thedanpetrov@gmail.com>
2023-01-04 22:16:31 +0100

    Bootstrappe Ametaverse page

---
+++ src/routes/ametaverse/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import type { PageData } from './$types';
+    
+    export let data: PageData;
+</script>
+
+<div class="w-screen bg-primary text-accent space-y-6 py-6">
+    <h1 class="text-center text-5xl font-bold font-norwester">Ametaverse</h1>
+    <p class="mx-5"><strong>Ametaverse</strong> sounds familiar, but is strange at the same time. The Ametaverse is a limitless world of possibility, offering endless opportunities for creativity and self-expression. Through the Ametaverse, we can experience and participate in virtual worlds that push the boundaries of what we thought was possible. It is a place where anyone can be Ame!
+    <br>
+        Please watch the official announcement keynote from our CEO below.
+    </p>
+    <iframe class="h-96 mx-auto" width="720" height="480" src="https://www.youtube.com/embed/umOghkJvrpQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+</div>
