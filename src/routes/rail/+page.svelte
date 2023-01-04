d775239b7dd6067af64d482b13e10af604b3bd92
Daniils Petrovs <thedanpetrov@gmail.com>
2023-01-05 00:46:12 +0100

    Add hyperloop map

--- src/routes/rail/+page.svelte
+++ src/routes/rail/+page.svelte
@@ -2,6 +2,8 @@
     import type { PageData } from './$types';
     import {Icon} from '@steeze-ui/svelte-icon';
     import {ArrowDown} from '@steeze-ui/heroicons';
+
+    import hyperloopMap from "$lib/assets/hyperloop.png";
     
     export let data: PageData;
 </script>
@@ -12,18 +14,19 @@
         <br><br>
     Please see our numerous successful projects below.
     </p>
-    <Icon src={ArrowDown} size="24px" theme="solid" class="mx-auto"/>
+    <Icon src={ArrowDown} size="24px" theme="solid" class="mx-auto text-accent"/>
 </div>
 
 <div class="sketchfab-embed-wrapper h-screen bg-accent text-primary">
     <h2 class="text-center text-4xl font-bold font-norwester py-5">Train Stops</h2>
     <iframe class="w-screen h-3/4" title="Ame Waits for the Train Home" frameborder="0" allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true" allow="autoplay; fullscreen; xr-spatial-tracking" xr-spatial-tracking execution-while-out-of-viewport execution-while-not-rendered web-share src="https://sketchfab.com/models/3add46c79d624f34b144b3391f6c54da/embed?dnt=1"> </iframe> <p style="font-size: 13px; font-weight: normal; margin: 5px;"> <a href="https://sketchfab.com/3d-models/ame-waits-for-the-train-home-3add46c79d624f34b144b3391f6c54da?utm_medium=embed&utm_campaign=share-popup&utm_content=3add46c79d624f34b144b3391f6c54da" target="_blank" style="font-weight: bold;"> Ame Waits for the Train Home </a> by <a href="https://sketchfab.com/priichu?utm_medium=embed&utm_campaign=share-popup&utm_content=3add46c79d624f34b144b3391f6c54da" target="_blank" style="font-weight: bold;"> Priichu </a> on <a href="https://sketchfab.com?utm_medium=embed&utm_campaign=share-popup&utm_content=3add46c79d624f34b144b3391f6c54da" target="_blank" style="font-weight: bold;">Sketchfab</a></p>
+    <Icon src={ArrowDown} size="24px" theme="solid" class="mx-auto text-primary"/>
 </div>
 
 <div class="bg-primary text-accent space-y-5 py-5 px-5">
     <h2 class="text-center text-4xl font-bold font-norwester">Hyperloop</h2>
     <p class="mx-5">An innovative form of transport between far away places in worlds like Minecraft. With a budget of nearly <strong>one Amellion Watbucks</strong>, it was the largest Watson Railways project to date. 
-        Watson Hyperloop connects far away places such as PPP with central Mythtown.</p>
+        Watson Hyperloop connects far away places such as PPP with central Headquarters.</p>
     <h3 class="text-center text-3xl font-bold font-norwester">Specs</h3>
     <table class="table-auto mx-5 sm:mx-auto">
         <thead>
@@ -48,8 +51,8 @@
         </tbody>
     </table>
     <h3 class="text-center text-3xl font-bold font-norwester">Map</h3>
-    <div class="text-center">
-        (subway map goes here)
+    <div>
+        <img src={hyperloopMap} alt="HoloEN Minecraft Hyperloop" class="sm:max-w-4xl md:max-w-2xl xl:max-w-6xl mx-auto shadow-md rounded" />
     </div>
     <h3 class="text-center text-3xl font-bold font-norwester">Construction</h3>
     <div class="flex flex-col">
