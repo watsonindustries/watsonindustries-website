a05702111e5164da511bc42bb31256186da73a7d
Daniils Petrovs <thedanpetrov@gmail.com>
2023-01-06 02:02:20 +0100

    Update credits

--- src/lib/CreditCard.svelte
+++ src/lib/CreditCard.svelte
@@ -9,7 +9,7 @@
 	<div class="card-body items-center text-center">
 		<h2 class="card-title">{name}</h2>
 		<p>{credit}</p>
-		{#if url}
+		{#if url && url !== ''}
 		<div class="card-actions">
 			<a type="button" href={url} class="btn btn-info transform transition hover:scale-105">{buttonText}</a>
 		</div>
--- src/routes/credits/+page.svelte
+++ src/routes/credits/+page.svelte
@@ -2,22 +2,34 @@
     import type { PageData } from './$types';
 
     import CreditCard from '$lib/CreditCard.svelte';
+
+    const imageryCredits = [
+        {name: 'Totangotan', credit: 'Uber Sheep Logo', url: 'https://twitter.com/totangtotan/status/1319319188129808385', buttonText: 'Twitter'},
+        {name: 'Spiritsnare & Takanashi Kiara', credit: 'KFP Logo', url: 'https://twitter.com/takanashikiara/status/1393734168559898624', buttonText: 'Twitter'},
+        {name: 'Walfie', credit: 'Smol Ame art', url: 'https://twitter.com/walfieee/timelines/621818256383442944', buttonText: 'Twitter'},
+        {name: 'Priichu', credit: '3D Ame art', url: 'https://sketchfab.com/priichu', buttonText: 'Sketchfab'},
+        {name: 'Irasutoya', credit: 'Images', url: 'https://www.irasutoya.com/', buttonText: 'Website'}
+    ]
+
+    const qaCredits = [
+        {name: 'Dominik K', credit: 'Testing and feedback', url: 'https://twitter.com/Dominik_KFBeats', buttonText: 'Twitter'},
+        {name: 'Moondog', credit: 'Assistance and testing', url: 'https://twitter.com/Moondog_94', buttonText: 'Twitter'},
+        {name: 'PONG', credit: 'Testing and feedback', url: '', buttonText: ''}
+    ]
     
     export let data: PageData;
 </script>
 
-<div class="text-center mx-auto bg-primary">
+<div class="text-center mx-auto bg-primary pb-4">
     <h1 class= "text-5xl text-accent py-4">Credits</h1>
 
     <h2 class="text-3xl text-accent font-bold py-4">
         Imagery
     </h2>
     <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-y-4 text-primary">
-        <CreditCard name="Totangotan" credit="Uber Sheep logo" url="https://twitter.com/totangtotan/status/1319319188129808385" buttonText="Twitter"></CreditCard>
-        <CreditCard name="Spiritsnare & Takanashi Kiara" credit="KFP Logo" url="https://twitter.com/takanashikiara/status/1393734168559898624" buttonText="Twitter"></CreditCard>
-        <CreditCard name="Walfie" credit="Smol Ame art" url="https://twitter.com/walfieee/timelines/621818256383442944" buttonText="Twitter"></CreditCard>
-        <CreditCard name="Priichu" credit="3D Ame art" url="https://sketchfab.com/priichu" buttonText="Sketchfab"></CreditCard>
-        <CreditCard name="Irasutoya" credit="Images" url="https://www.irasutoya.com/" buttonText="Website"></CreditCard>
+        {#each imageryCredits as credit}
+            <CreditCard name={credit.name} credit={credit.credit} url={credit.url} buttonText={credit.buttonText}></CreditCard>
+        {/each}
     </div>
 
     <h2 class="text-3xl text-accent font-bold py-4">
@@ -32,4 +44,13 @@
         <CreditCard name="ChatGPT by OpenAI" credit="Parts of text content" url="https://openai.com/blog/chatgpt"></CreditCard>
     </div>
 
+    <h2 class="text-3xl text-accent font-bold py-4">
+        QA and Testing
+    </h2>
+
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-y-4 text-primary">
+        {#each qaCredits as credit}
+            <CreditCard name={credit.name} credit={credit.credit} url={credit.url} buttonText={credit.buttonText}></CreditCard>
+        {/each}
+    </div>
 </div>
